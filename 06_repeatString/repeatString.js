219844b7d1d389e503cb6a14d72b5a966fa1e584
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-06-16 15:09:18 -0700

    completed exercises 03-06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if(count < 0)
+        return "ERROR";
+    let result = "";
+    
+    for( let i = 0; i < count; i++)
+    {
+       result += str;
+    }
+    
+    return result;
+
+    // if(count >= 0)
+    //     return str.repeat(count);
+    // else
+    //     return 'ERROR';
 
 };
 
