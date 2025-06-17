1029554bb53f12b037ba9733fe7b38dc15f7d43e
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-06-16 23:19:10 -0700

    finished exercises 07-09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+    let sum = 0;
+    if ( !Number.isInteger(int1) || !Number.isInteger(int2))
+        return 'ERROR';
+    if(int1 < 0 || int2 < 0)
+        return 'ERROR';
+
+    if (int1 > int2)
+    {
+        let temp = int1;
+        int1 = int2;
+        int2 = temp;
+    }
+
+    for(let i = int1; i <= int2; i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
