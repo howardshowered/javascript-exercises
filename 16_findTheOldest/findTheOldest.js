f52bd1ac93f14418d7a27a0f44fda7aafa245088
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-06-21 16:12:10 -0700

    finished exercises 14-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    let oldest = 0;
+    let result;
+    const currentDate = new Date();
+
+    for(let person of arr)
+    {
+
+        let currentPersonAge;
+
+        if( person.yearOfDeath === undefined)
+        {
+            currentPersonAge = currentDate.getFullYear() - person.yearOfBirth;
+        }
+        else
+            currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+
+        if(oldest < currentPersonAge)
+        {
+            oldest = currentPersonAge;
+            result = person;
+        } 
+            
+
+    }
+
+    return result;
 };
 
 // Do not edit below this line
